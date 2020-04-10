d9038b23d0235a7a64e1a9b7c5fa07e9ee9dca70
Louis Gauthron <louisgauthron@mathieus-air-2.home>
2020-04-10 16:49:17 +0100

    line function

--- main.js
+++ main.js
@@ -1,52 +1,31 @@
-
-
 var canvas = document.getElementById('canvas');
-var clickX = [0, 450, 200, 50];
-var clickY = [0, 200, 450, 50];
-var clickDrag = [false, true,false,true];
 var context = canvas.getContext("2d");
 context.lineCap = 'round';
 context.lineJoin = 'round';
-context.strokeStyle = 'red';
+context.strokeStyle = 'green';
 context.lineWidth = 1;
 
-var redraw = function () {
-    for (var i = 0; i < clickX.length; ++i) {
-        context.beginPath();
-        if (clickDrag[i] && i) {
-            context.moveTo(clickX[i - 1], clickY[i - 1]);
-        }
-        else {
-            context.moveTo(clickX[i] - 1, clickY[i]);
-        }
-        context.lineTo(clickX[i], clickY[i]);
-        context.stroke();
-    }
-    context.closePath();
-};
 var clearCanvas = function () {
     context
         .clearRect(0, 0, this.canvas.width, this.canvas.height);
 
 };
 
-
-var plus = function(a , b) {
-    return a + b
+var line = function(x1, y1, x2, y2, colour){
+    context.strokeStyle = colour;
+    context.beginPath();
+    context.moveTo(x1,y1);
+    context.lineTo(x2,y2);
+    context.stroke();
 }
 
 var redrawLouis = function() {
-    //context.beginPath();
-    context.moveTo(200,200);
-    context.lineTo(300,300);
-    context.stroke();
-
-    context.moveTo(0,200);
-    context.lineTo(100,300);
-    context.stroke();
- 
-    //context.closePath();
+    line(200, 0, 200, 490,'yellow')    
+    line(0, 200, 100, 200,'red')
 }
-
+ 
 clearCanvas()
 redrawLouis()
+
+line(0, 5000, 300, 300,'green')
+line(300, 200, 4000, 300,'purple')
